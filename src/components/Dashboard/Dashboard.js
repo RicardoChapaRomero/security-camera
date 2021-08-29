1a12ebcea9e1dedd9f521418ea9ea858bdcb64ad
zJuarez <mji2000.741@gmail.com>
2021-08-28 22:58:26 -0500

    table update

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,39 +1,82 @@
-import React, { Component, useMemo } from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import Table from './AppTable/AppTable';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import { ChevronDown, ChevronUp } from "heroicons-react";
 
 /** This class handles the latest data retrieved from the camera detections
  * TODO(any): Define what data is going to be shown in the UI.
  */
 function Dashboard() {
-    const kHomeViewColumns = useMemo(() => [
-      {
-        Header: "Latest detections",
-        columns: [
-          {
-            Header: "Name",
-            accessor: "detection.name"
-          },
-          {
-            Header: "Total number of appearances",
-            accessor: "detection.appearances"
-          },
-          {
-            Header: "Latest appearance",
-            accessor: "detection.latest_timestamp"
-          }
-        ]
-      }
-    ], []);
 
+  const [selectedAzureId, setSelectedAzureId] = useState(-1);
+  const columns = [{
+    dataField: 'name',
+    text: 'Name',
+    sort: true,
+    sortCaret: (order, column) => {
+      if (!order) return <ChevronDown/>;
+    else if (order === 'asc') return <ChevronUp/>;
+    else if (order === 'desc') return <ChevronDown/>;
+    return null;
+    }
+  }, {
+    dataField: 'appearances',
+    text: 'Appearances',
+    sort: true,
+    sortCaret: (order, column) => {
+      if (!order) return <ChevronDown/>;
+    else if (order === 'asc') return <ChevronUp/>;
+    else if (order === 'desc') return <ChevronDown/>;
+    return null;
+    }
+
+  }, {
+    dataField: 'latest_timestamp',
+    text: 'Latest timestamp',
+    sort: true,
+    sortCaret: (order, column) => {
+      if (!order) return <ChevronDown/>;
+    else if (order === 'asc') return <ChevronUp/>;
+    else if (order === 'desc') return <ChevronDown/>;
+    return null;
+    }
+
+  }];
     
     const kMockData = [
       {
+        "id": 1,
+        "azure_id": 23234314,
         "name": 'Jamir',
         "appearances": 3,
         "latest_timestamp": "28/08/2021"
+      },
+      {
+        "id": 2,
+        "azure_id": 23234340,
+        "name": 'Zeta',
+        "appearances": 2,
+        "latest_timestamp": "27/08/2020"
+      },
+      {
+        "id": 3,
+        "azure_id": 23434,
+        "name": 'Chapa',
+        "appearances": 5,
+        "latest_timestamp": "27/08/2019"
       }
     ];
+    const selectRow = {
+      mode: 'radio',
+      clickToSelect: true,
+      hideSelectColumn: true,
+      style: (row, rowIndex) => {
+        const backgroundColor = '#00BFFF';
+        setSelectedAzureId(row.azure_id);
+        return { backgroundColor };
+      }
+    };
       return(
         <Col sm={5} className='data-wrapper d-flex justify-content-center pt-5'>
           <Container>
@@ -44,7 +87,7 @@ function Dashboard() {
             </Row>
             <Row>
               <div className='info-table-wrapper'>
-                <Table columns={ kHomeViewColumns } data={ kMockData }/>
+              <BootstrapTable keyField='id' data={ kMockData } columns={ columns } selectRow = {selectRow}/>
               </div>
             </Row>
           </Container>
